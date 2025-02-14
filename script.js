9a6e12f5f914860c4709509b8b7b68cf8becef25
Nasroo <63188489+Nasroo@users.noreply.github.com>
2025-02-14 21:48:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-// Function to fetch message from JSON file
-function fetchMessage() {
-    fetch('message.json')
-        .then(response => response.json())  // Parse JSON response
-        .then(data => {
-            document.getElementById('message').innerText = data.message;
-            document.getElementById('message').style.display = 'block';
-        })
-        .catch(error => console.error('Error fetching message:', error));
+// Function to show the Valentine's message
+function showValentineMessage() {
+    document.getElementById('message').innerText = "أحب وجودك.. وأعشق كيانكِ في حياتي\nأنا في غرامك\nأنا في غرامك، أعيش كل لحظة متأملًا في تفاصيلك، في تفاصيل كيانك، ولم يعد للعالم معنى دونك.. كل لحظة من حياتي أصبحت أجمل بوجودك، وأنت وحدك من يملك مفتاح قلبي.. لم أتخيل قط أن أكون تحت حبٍّ لم يسبق له وجودٌ، وأنا سعيد وممتنٌّ.. ليس لمرئٍ حظٌّ كحظّي أن تكون لي جوهرةٌ يصطفُّ لها كل من له بصيرة.. أنا حظّي أكبر من أن يتخيله إنسٌ.. أن تكوني لي وأكون لكِ\n\nأنام وأحلم، أستيقظ وأحلم.. أحلم باليوم الذي تكوني فيه معي بجنبي.. أعدّ الأيام تعدادا ويا ليت العدّ ينتهي يوما، لأبدأ العدّ مجدّدا.. لنبدأ العدّ.. مجدّدا.. معا..\n\nأريد أن أقبلك، أن تلتقي شفاهنا في لحظة تذوب فيها كل المسافات.. كرهت المسافات ولكن ليس قدر ما أحببت حبي لكِ، عشقي أسرني في سجن لا أتمنى الخروج منه، بل زيارتي من خلف القضبان تبعث في العزيمة والقدرة لفتح عينيّ في الصباح.. سهامٌ؟ هي سهامٌ تألمني حينما ليست فيّ منغرسة..\nأريد أن أقبلك.. أن أذوب في طعمك وأحس بأسرار قلبك تنبض في كل قبلة كما لو أن الزمان يتوقف لحظتها، ويصبح العالم كله محصورًا في تلك اللمسة..\n\nأحبّ حين تناديني عزيزي.. يغمرني شعور بالانتماء\n\nكأنّي طفل.. لاحول ولاقوة لي.. أذوب بين الخيال والحقيقة.. أريد سماع عزيزي من لحنك قبل أن تقبلي ٱلامي.. وتشعريني أنّي.. أنّني على قيد الحياة\n\nكلمات.. كلمات تخرج من داخلي.. باطنٌ ممتلئٌ.. يخرج على شكل حروف تحاول تهجئة كيان الحبّ الذي لا تهجئة له.. عشقٌ.. كأنّ وجوده محجوزٌ بين حدود أفق ثقب أسود نوره لا يكاد يتجلى.. ولكن نور عشقي لك يشعّ أقوى من شمعة في زجاجة.. باقٍ غير فانٍ..\n\nفي بحر عينيك هامت كل أشواقي يا ربة الحسن هل تنوين إغراقي ما كنت أؤمن بالعيون وفعلها حتى دهتني بالهوى عيناك";
+    document.getElementById('message').style.display = 'block';
 }
 
 // Add event listener to the original Yes button
-document.getElementById('yesBtn').addEventListener('click', fetchMessage);
+document.getElementById('yesBtn').addEventListener('click', showValentineMessage);
 
 // Handle No button click
 document.getElementById('noBtn').addEventListener('click', function() {
@@ -27,5 +22,7 @@ document.getElementById('okBtn').addEventListener('click', function() {
 
     // Remove previous event listener (No button function)
     noBtn.replaceWith(noBtn.cloneNode(true));
-    document.getElementById('yesBtn2').addEventListener('click', fetchMessage);
+    document.getElementById('yesBtn2').addEventListener('click', showValentineMessage);
 });
+
+
